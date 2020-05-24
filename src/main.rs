2c96692aa5b0773e2dd05c0067536084395f94df
tims <tsteeman@jibes.nl>
2020-05-24 05:57:27 +0200

    implemented functions for reading file in first arg

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
 extern crate csv;
+use std::env;
 use std::error::Error;
-use std::io;
 use std::process;
+use std::ffi::OsString;
+use std::fs::File;
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let file_path = get_first_arg()?;
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
 
 fn main() {
     if let Err(err) = run() {
@@ -13,11 +35,3 @@ fn main() {
    
 }
 
-fn run() -> Result<(), Box<dyn Error>>{
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
-}
